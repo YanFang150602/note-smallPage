4bea9b77ea7b8af6b89fc9087a0e21abaa78087a
yanff <838641570@qq.com>
2019-03-10 17:08:04 +0800

    ok

---
+++ vue/vueprojectdemo/src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <div style="float:left;width:240px;margin-top:240px;">
+      <ul>
+        <li>
+          <!-- 字符串 -->
+          <router-link to="/">Home</router-link>
+        </li>
+        <li>
+          <!-- 字符串 -->
+          <router-link to="/student">Student1</router-link>
+        </li>
+        <li>
+          <!-- 使用 v-bind 的 JS 表达式 -->
+          <router-link v-bind:to="'student'">Student</router-link>
+        </li>
+      </ul>
+    </div>
+    <div>
+      <router-view/>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  mounted:function(){
+    console.log(this);
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: block;
+  margin: 0 10px;
+  text-align: left;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ vue/vueprojectdemo/src/components/student/StudentList.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <table>
+      <thead>
+        <th>
+          <td>
+          {{L["table.id"]}}
+          </td>
+          <td>
+          <!-- {{$t('L.table.name')}} -->
+          </td>
+          <td>
+          <!-- {{$t('L.table.age')}} -->
+          </td>
+          <td>
+          <!-- {{$t('L.table.grade')}} -->
+          </td>
+          <td>
+          <!-- {{$t('L.table.address')}} -->
+          </td>
+          <td>
+          <!-- {{$t('L.table.tephone')}} -->
+          </td>
+        </th>
+      </thead>
+      <tbody>
+        <tr v-for="student in studentList">
+          <td v-html="student.id"></td>
+          <td v-html="student.name"></td>
+          <td v-html="student.age"></td>
+          <td v-html="student.grade"></td>
+          <td v-html="student.address"></td>
+          <td v-html="student.telphone"></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "StudentList",
+    data(){
+      return {
+        studentList: [
+          {
+            id: '1903101618',
+            name: 'Huang Xiaoming',
+            age: 20,
+            grade: 'Three Grade',
+            address: 'NanJing',
+            telphone: '13914774571'
+          }
+        ]
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>

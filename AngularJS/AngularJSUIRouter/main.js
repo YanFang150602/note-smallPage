4c9bd29c2545ca86dec4ae43e93df720d1d66b2b
颜泽芳 <838641570@qq.com>
2020-11-13 10:10:23 +0800

    Update main.js

--- AngularJS/AngularJSUIRouter/main.js
+++ AngularJS/AngularJSUIRouter/main.js
@@ -1,7 +1,8 @@
 require(['js/cmn'],function(module){
     require([
         'js/controllers/homeController',
-        'js/controllers/blogController'
+        'js/controllers/blogController',
+        'js/controllers/newsController'
     ], function(){
         angular.element(document).ready(function() {
           angular.bootstrap(document, [module.name]);   

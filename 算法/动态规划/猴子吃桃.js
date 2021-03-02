508194d26c8d0c2872898c766c0f2801d24f94c3
颜泽芳 <838641570@qq.com>
2021-03-02 11:07:38 +0800

    Update 猴子吃桃.js

--- 算法/动态规划/猴子吃桃.js
+++ 算法/动态规划/猴子吃桃.js
@@ -1,42 +1,38 @@
 process.stdin.resume();
 process.stdin.setEncoding('utf-8');
 
-let input = '';
-process.stdin.on('data', data => {
-  input += data;
-  setTimeout(() => {
-    process.stdin.emit('end');
-  }, 10000);
+/**
+ * 82. 猴子爬山
+ * 每一次只能跳1步或跳3步，试问猴子通过这个阶梯有多少种不同的跳跃方式
+ * dp[i] = dp[i-1] + dp[i-3]
+ */
+
+var input;
+process.stdin.on('data', (data) => {
+    input += data;
+    setTimeout(()=>{
+        process.stdin.emit('end');
+    },5000);
+});
+process.stdin.on('end', () => {
+    let inputArray = input.split('\n');
+    const output = [];
+    let dp = [];
+    dp[0] = 0;
+    dp[1] = 1;
+    dp[2] = 1;
+    dp[3] = 2;
+    inputArray.forEach((N) => {
+        for(let i = 4; i <= N; i++){
+            dp[i] = dp[i-1] + dp[i-3];
+        }
+        output.push(dp[N]);
+    })
+    console.log(`${output.join('\n')}`);
+    process.exit();
 });
 
-process.stdin.on('end', () => {
-  const row = input.trim();
-  const datas = row.split(' ');
-  const hour = datas.splice([datas.length - 1], 1)[0];
-  if (isNaN(hour)) {
-    console.log(-1);
-  } else {
-    for (let minSpeed = 1; ;minSpeed++) {
-      let totalHour = 0;
-      let isError = false;
-      for (let num of datas) {
-        if (isNaN(num)) {
-          isError = true;
-          break;
-        }
-        num = Number(num);
-        totalHour += Math.round(num / minSpeed);
-      }
-      if (totalHour <= hour) {
-        console.log(minSpeed);
-        break;
-      }
-      if (isError) {
-        console.log(-1);
-        break;
-      }
-    }
-  }
-  
-  process.exit();
+
+process.on('exit', function (code) {
+    console.log('退出\(^o^)/~');
 });
